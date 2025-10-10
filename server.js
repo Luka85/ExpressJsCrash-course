ef7d5d1f5cd451a4be7847b23ceffb6a32105964
Luka Kikelj <kikelj.luka@gmail.com>
2025-10-10 05:01:16 +0200

    build static server and work with res.json

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-app.get("/about", (req, res) => {
-  res.send("About");
-});
+//setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+let posts = [
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post two" },
+  { id: 3, title: "Post three" },
+];
 
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 app.listen(8000, () => console.log("Server is running on port 8000"));
